57bc72c6a32a355eb95d1b495dc650b9e269bed9
Tom <tom.christ.99@gmail.com>
2024-04-20 23:35:15 +0200

    Added C and Cpp template module / class files

---
+++ .template/TemplateClass.cpp
@@ -0,0 +1,12 @@
+/*!
+ * @file 	    TemplateClass.cpp
+ * @brief 	    TODO
+ * @author 	    Tom Christ
+ * @date 	    2024-04-20
+ * @copyright   Copyright (c) 2024 Tom Christ; MIT License   
+ * @version	    0.1		Initial Version
+ */
+
+#include "TemplateClass.h"
+
+#define TAG "TemplateClass"
---
+++ .template/TemplateClass.h
@@ -0,0 +1,17 @@
+/*!
+ * @file 	    TemplateClass.h
+ * @brief 	    TODO
+ * @author 	    Tom Christ
+ * @date 	    2024-04-20
+ * @copyright   Copyright (c) 2024 Tom Christ; MIT License   
+ * @version	    0.1		Initial Version
+ */
+
+#ifndef TemplateClass_H_
+#define TemplateClass_H_
+
+class TemplateClass{
+
+};
+
+#endif /* TemplateClass_H_ */
